3d945c0877c40f6fba62d4ba69f3729799b11bc1
Amadeus Alexander <amadeus12321@gmail.com>
2021-08-04 21:32:05 +0800

    month

--- Client/methods.js
+++ Client/methods.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Moment = require('moment-timezone')
 let userTeleId = undefined
 const category = ["Work Food", "Good Food", "Coffee", "Alcohol", "Necessities", "Shopping & Leisure", "Ciggs", "Private Transport", "Groceries" ]
 async function getMontlyExpenseReport(telegramId) {
@@ -11,7 +12,28 @@ async function getMontlyExpenseReport(telegramId) {
                 report += res.data[i]['Category'] + ": " + "$" + res.data[i]['Total'].toFixed(2) + "\n"
                 total += parseFloat(res.data[i]['Total'])
             }
-            return report + "\nTotal: $" + total.toFixed(2)
+            const currentTime = Moment().tz('Asia/Singapore')
+            const month = String(currentTime.month() + 1).padStart(2, '0')
+            var dict = new Object()
+            dict = {
+              1 : "January",
+              2 : "Feburary",
+              3 : "March",
+              4 : "April",
+              5 : "May",
+              6 : "June",
+              7 : "July",
+              8 : "August",
+              9 : "September",
+              10 : "October",
+              11 : "November",
+              12 : "December"
+            }
+            console.log(dict[2])
+            console.log(parseInt(month))
+            console.log(dict[parseInt(8)])
+            console.log(dict[parseInt(month)])
+            return "This is your monthly expenses for " + dict[parseInt(month)] + ".\n" + report + "\nTotal: $" + total.toFixed(2)
         }
       } catch (error) {
         console.error(error);
